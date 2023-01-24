b0211911c2e42d4880237c6dd6a70e8c50701743
Varala-Sirisha-492 <112552647+Varala-Sirisha-492@users.noreply.github.com>
2023-01-24 23:07:10 +0530

    Update Whitespaces.c

--- Whitespaces.c
+++ Whitespaces.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main()
 {
-char str[100];
-int i;
-int space=0;
-    printf("Enter a string\n");
-    gets(str);
-    i=0;
-    do{
- if(str[i]==' '){
-        space++;
+    char str[100];//input string with size 100
+
+    int words=0,newline=0,characters=0; // counter variables
+
+    scanf("%[^~]",&str);//scanf formatting    
+
+    for(int i=0;str[i]!='\0';i++)
+     { 
+         if(str[i] == ' ')
+         { 
+              words++;
+         }
+         else if(str[i] == '\n')
+         {
+             newline++;
+              words++;//since with every next line new words start. corner case 1
+         }
+         else if(str[i] != ' ' && str[i] != '\n'){
+         characters++;
+         }
+     }
+    if(characters > 0)//Corner case 2,3.
+    {
+        words++;
+        newline++;
     }
-    i++;
-}while(i<=str[i]);
-printf("Total white space :%d ",space);
+     printf("Total number of words : %d\n",words);
+     printf("Total number of lines : %d\n",newline);
+     printf("Total number of characters : %d\n",characters);
     return 0;
 }
